aa5c09480546d5d3d57ae049d3bbd1c0e717baf8
Foolz <akufoolz@gmail.com>
2019-11-11 22:30:12 -0700

    handled ValueError

--- pompom.py
+++ pompom.py
@@ -4,7 +4,7 @@ from time import sleep
 from pymsgbox import alert
 
 def interval(num, desc):
-    for _ in tqdm(range(int(num) * 60), desc=desc):
+    for _ in tqdm(range(int(num) * 60), desc=str(desc)):
         sleep(1)
     alert('{} has completed!'.format(desc), '{} Alert'.format(desc))
 
@@ -13,15 +13,17 @@ def getArgs():
     return argsList
 
 def pomodoro(args):
-    a = args[1]
-    b = args[2]
-    task = args[3]
-    interval(a, task)
-    interval(b, "Break")
+    args = getArgs()
+    try:
+        for _ in range(int(args[3])):
+            interval(args[1], args[4])
+            interval(args[2], "Break")
+    except ValueError:
+        print("Please provide whole numbers for the interval lengths.")
+        return 
 
 def main():
-    while True:
-        pomodoro(getArgs())
+    pomodoro(getArgs())
 
 if __name__ == "__main__":
     main()
